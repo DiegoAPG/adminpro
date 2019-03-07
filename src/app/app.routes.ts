5d70d9f139bac808d1cb2e4f520e5299f13a3a4f
DiegoAPG <diegoarm@gmail.com>
2019-03-06 19:18:19 -0600

    modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
-
-
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
     // las rutas sin path mandan a PagesComponent donde tiene subrutas y su propio app-outlet
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ]
-    },
+    // {
+    //     path: '',
+    //     component: PagesComponent,
+    //     children: [
+    //         { path: 'dashboard', component: DashboardComponent },
+    //         { path: 'progress', component: ProgressComponent },
+    //         { path: 'graficas1', component: Graficas1Component },
+    //         { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    //     ]
+    // },
     // las rutas siguientes mandaran a appComponent donde tiene un app-outlet que carga siguientes rutas
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
